712ad819f0d8b54f262963fae5cc4580cdc8bd65
tanmancan <contact@tkarimdesign.com>
2014-07-23 22:06:52 -0500

    Added ability to manulay adjust what time to show when static time setting is turned on.

--- js/settings.js
+++ js/settings.js
@@ -13,15 +13,40 @@ var clockSettings = {
 	
 	initSettings: function(){
 
+		//All settings
 		$('#showtime').is(':checked') ? this.opts.dispTime = 'on' : this.opts.dispTime = 'off';
 		$('#statictime').is(':checked') ? this.opts.staticTime = 'on' : this.opts.staticTime = 'off';
 		
+
+		//Custom options per settings
+
+		//Static time manual adjustment
+		//Update static value or hide the manual adjustment inputs
+		$('#statictime').is(':checked') ? this.staticTimeManual() : $('.staticInput').hide();
+
+
 		return this;
 
 	},
+
+	staticTimeManual: function(){
+
+		$('.staticInput').show();
+		var hr = hr = $('input.staticHours').val(),
+			min = $('input.staticMinutes').val();
+
+		//Workaround for buggy default val retrieval when using input[number]
+		!hr ? hr = 12 : hr = $('input.staticHours').val();
+		!min ? min = 25 : min = $('input.staticMinutes').val();
+
+		this.opts.staticSetTime = 'Thu, 01 Jan 1970 '+hr+':'+min+':00';
+	},
+
 	applySettings: function(){
 
+		//Initialize the settings
 		this.initSettings();
+		//Refresh the clock
 		startClock(this.opts);
 		return this;
 	}
@@ -30,6 +55,12 @@ var clockSettings = {
 
 
 $('.switch').on('click', function(){
-
+	console.log($('input.staticHours').val());
+	console.log($('input.staticMinutes').val());
 	clockSettings.applySettings();
+});
+
+$('.staticInput input').on('change', function(){
+	console.log('val changed');
+	clockSettings.applySettings();	
 });
